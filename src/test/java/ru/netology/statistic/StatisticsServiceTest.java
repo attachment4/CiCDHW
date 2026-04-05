d41b5ae90a85f96f451bd264c0dbfe51724b4572
attachment4 <karavashkinivan1995@gmail.com>
2026-04-05 08:19:14 -0800

    Change StaticServicesTest

---
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -0,0 +1,30 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StatisticsServiceTest {
+
+    @Test
+    void findMaxWhenMaxInMiddle() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomes = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomes);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMax2() {
+        StatisticsService service = new StatisticsService();
+        long [] incomes = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12, 13};
+
+        long expected = 13;
+        long actual = service.findMax(incomes);
+
+        assertEquals(expected, actual);
+    }
+}
